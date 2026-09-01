24ec61ff6809762cd6f11ce7a0c51e1915a76411
JimboRosso <marianstarclub@gmail.com>
2026-09-01 13:41:54 -0300

    Modifico main: Pido al usuario que ingrese la ruta del archivo y segun la extension depende la clase instanciada

--- src/Estructura_Proyecto/ProyectoMain.java
+++ src/Estructura_Proyecto/ProyectoMain.java
@@ -1,5 +1,25 @@
 package Estructura_Proyecto;
-
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.Scanner;
 public class ProyectoMain {
-    
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la ruta del archivo: ");
+        String caminoEntrada = scanner.nextLine();
+
+        ImageBuffer.imageBuffer buffer;
+
+        String extension = caminoEntrada.substring(caminoEntrada.lastIndexOf('.') + 1).toLowerCase();
+
+        if (extension.equals("jpg") || extension.equals("png")) {
+            buffer = new AdapterImagenJVM(caminoEntrada);
+
+        } else if (extension.equals("pgm") || extension.equals("txt")) {
+            buffer = new AdapterImagenPGM(caminoEntrada);
+
+        } else {
+            System.out.println("Formato no soportado.");
+        }
+    }
 }
