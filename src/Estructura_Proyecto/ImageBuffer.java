106b4ed7749be17d11dcb879f0e61c1ad7626dac
ALex Urquiza <alex.urquiza@est.fi.uncoma.edu.ar>
2026-09-01 12:01:34 -0300

    Implementacion de la clase ImageBuffer, se creo y se declaro la interfaz Target

--- src/Estructura_Proyecto/ImageBuffer.java
+++ src/Estructura_Proyecto/ImageBuffer.java
@@ -1,5 +1,20 @@
 package Estructura_Proyecto;
 
 public class ImageBuffer {
-    
+    //Interfaz Target (Patron Adapter)
+    public interface imageBuffer {
+
+        //Arreglo unidimensional de pixeles que representa la imagen
+        int[] getPixeles();
+        
+        //Ancho de la imagen
+        int getAncho();
+
+        //Altura de la imagen
+        int getAltura();
+
+        //Metodo para guardar la imagen en un archivo
+        void guardar(String rutaSalida) throws Exception;
+    }
+
 }
