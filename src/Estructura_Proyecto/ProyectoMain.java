d557eb7ef462d0137037aa737c27a268cafab756
JimboRosso <marianstarclub@gmail.com>
2026-09-01 14:25:01 -0300

    Este si es el main modificado

--- src/Estructura_Proyecto/ProyectoMain.java
+++ src/Estructura_Proyecto/ProyectoMain.java
@@ -4,11 +4,13 @@ import java.util.concurrent.RecursiveAction;
 import java.util.Scanner;
 public class ProyectoMain {
     public static void main(String[] args) throws Exception {
+        //
         Scanner scanner = new Scanner(System.in);
+        boolean seguir = true;
         System.out.print("Ingrese la ruta del archivo: ");
         String caminoEntrada = scanner.nextLine();
 
-        ImageBuffer.imageBuffer buffer;
+        ImageBuffer.imageBuffer buffer = null;
 
         String extension = caminoEntrada.substring(caminoEntrada.lastIndexOf('.') + 1).toLowerCase();
 
@@ -20,6 +22,27 @@ public class ProyectoMain {
 
         } else {
             System.out.println("Formato no soportado.");
+            seguir = false;
+        }
+        if (seguir) {
+            System.out.println("Ingrese el filtro que desea aplicar: ");
+            System.out.println("1. Invertir colores");
+            System.out.println("2. Escala de grises");
+            System.out.println("3. Subir brillo (40 unidades)");
+            int opcion = scanner.nextInt();
+            FiltroImagen.TipoFiltro filtro;
+            if (opcion == 1) {
+                filtro = FiltroImagen.TipoFiltro.INVERTIR;
+            }else if (opcion == 2) {
+                filtro = FiltroImagen.TipoFiltro.ESCALA_GRIS;
+            }else {
+                filtro = FiltroImagen.TipoFiltro.BRILLO;
+            }
+            ForkJoinPool pool = new ForkJoinPool();
+            pool.invoke(new FiltroImagen(buffer.getPixeles(), 0, buffer.getPixeles().length, filtro));
+            buffer.guardar("resultado." + extension);
+            scanner.close();
+            
         }
     }
 }
