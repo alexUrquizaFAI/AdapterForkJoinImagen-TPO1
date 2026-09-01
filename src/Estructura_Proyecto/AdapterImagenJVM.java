467f5444d626b26edd03a30641b01d649caad27d
ALex Urquiza <alex.urquiza@est.fi.uncoma.edu.ar>
2026-09-01 13:10:06 -0300

    Implementacion del AdapterImagenJVM. Se toma la ruta de la imagen original, se extrae de la matriz de pixeles y se convierte en en un arrgelo unidimensional

--- src/Estructura_Proyecto/AdapterImagenJVM.java
+++ src/Estructura_Proyecto/AdapterImagenJVM.java
@@ -1,5 +1,66 @@
 package Estructura_Proyecto;
 
-public class AdapterImagenJVM {
+//Lo uso para almacenar la cuadricula de pixeles y acceder a sus propiedades visuales
+import java.awt.image.BufferedImage;
+//Lo uso para verificar si el archivo existe y para indicar donde leer o guardar
+import java.io.File;
+//Lo uso para leer y escribir formatos de imagen estandar como JPG o PNG
+import javax.imageio.ImageIO;
+
+
+public class AdapterImagenJVM implements ImageBuffer.imageBuffer { 
     
+    //Variable que nos permitira acceder a las propiedades visuales de la imagen
+    private BufferedImage imagenOriginal;
+
+    private int[] pixeles;
+    private int ancho; 
+    private int altura;
+
+    public AdapterImagenJVM(String rutaImagen) throws Exception {
+        
+        // Cargar la imagen utilizando ImageIO
+        File archivoImagen = new File(rutaImagen);
+        if (!archivoImagen.exists()) {
+            throw new Exception("El archivo de imagen no existe: " + rutaImagen);
+        }
+        
+        //Decodificamos el archivo 
+        this.imagenOriginal = ImageIO.read(archivoImagen);
+
+        if (this.imagenOriginal == null) {
+            throw new Exception("Formato de imagen no soportado.");
+        }
+
+        //Obtenemos el ancho de la imagen original
+        this.ancho = this.imagenOriginal.getWidth();
+        //Obetenemos la altura de la imagen original
+        this.altura = this.imagenOriginal.getHeight();
+        //Extraemos los colores de la imagen bidimensional(laImagenOriginal) y lo guardamos en un arrgelo unidimensional
+        this.pixeles = this.imagenOriginal.getRGB(0, 0, this.ancho, this.altura, null, 0, this.ancho);
+    }
+
+    @Override
+    public int[] getPixeles() {
+        return this.pixeles;
+    }
+
+    @Override
+    public int getAncho() {
+        return this.ancho;
+    }
+
+    @Override
+    public int getAltura() {
+        return this.altura;
+    }
+
+    public void guardar(String rutaSalida) throws Exception {
+        //Tomamos el arreglo de pixeles ya modificado y lo devolvemos a la imagen original
+        this.imagenOriginal.setRGB(0, 0, this.ancho, this.altura, null, 0, this.ancho);
+        //Obtenemos el tipo de archivo, si es png o jpg
+        String formato = rutaSalida.substring(rutaSalida.lastIndexOf(".") + 1);
+        //Guardamos en el disco
+        ImageIO.write(imagenOriginal, formato, new File(rutaSalida));
+    }
 }
--- src/Estructura_Proyecto/FiltroImagen.java
+++ src/Estructura_Proyecto/FiltroImagen.java
@@ -1,8 +1,9 @@
 package Estructura_Proyecto;
 //Xime
 import java.util.concurrent.RecursiveAction;
+
 public class FiltroImagen extends RecursiveAction {
-    private static final int UMBRAL= 100000;
+    private static final int UMBRAL = 100000;
 	private final int[] pixeles;
 	private final int inicio;
 	private final int fin;
@@ -29,25 +30,25 @@ public class FiltroImagen extends RecursiveAction {
         }
     }
     public void aplicarFiltro(){
-        for(int i = inicio; i<fin; i++){
+        for(int i = inicio; i < fin; i++){
             int argb = pixeles[i];
             //Extraigo el valor de cada color con manipulacion de bits (muy util la vdd)
-            // cada color comprende un valor entre 0 y 255, por lo que se puede representar con 8 bits (1 byte)
-            int a = (argb>>24) & 0xFF;
-            int r = (argb>>16) & 0xFF;
-            int g = (argb>>8) & 0xFF;
+            //cada color comprende un valor entre 0 y 255, por lo que se puede representar con 8 bits (1 byte)
+            int a = (argb >> 24) & 0xFF;
+            int r = (argb >> 16) & 0xFF;
+            int g = (argb >> 8) & 0xFF;
             int b = argb & 0xFF;
 
             switch(filtro){
                 case INVERTIR:
-                    r = 255-r;
-                    g= 255-g;
-                    b= 255-b;
+                    r = 255 - r;
+                    g = 255 - g;
+                    b = 255 - b;
                     break;
                 case ESCALA_GRIS:
                 //investigado: porcentaje de percepcion de colores (r=29.9%; g=58.7%; b=11.4%)
-                    int gris = (int)((0.299*r)+(0.587*g)+(0.114*b));//Esto es para obtener la tonalidad del gris
-                    r=g=b=gris;
+                    int gris = (int) ( (0.299 * r) + (0.587 * g) + (0.114 * b));//Esto es para obtener la tonalidad del gris
+                    r= g = b = gris;
                     break;
                 case BRILLO:
                 //Subir el brillo a cada color intentando no superar el maximo de 255
@@ -58,7 +59,7 @@ public class FiltroImagen extends RecursiveAction {
                     break;
             }
             //Reconstruimos el valor de color con manipulacion de bits
-            pixeles[i] = (a<<24) | (r<<16) | (g<<8) | b;
+            pixeles[i] = (a << 24) | (r << 16) | (g << 8) | b;
         }
     }
 
