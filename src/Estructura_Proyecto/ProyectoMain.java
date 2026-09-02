0ca2f9e4d11aa9997966329522360e3159f18b78
JimboRosso <marianstarclub@gmail.com>
2026-09-01 23:50:12 -0300

    Tratando de tener el mismo repositorio parte 2

--- src/Estructura_Proyecto/ProyectoMain.java
+++ src/Estructura_Proyecto/ProyectoMain.java
@@ -1,11 +1,9 @@
 package Estructura_Proyecto;
-
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
 import java.util.Scanner;
-
 public class ProyectoMain {
     public static void main(String[] args) throws Exception {
+        //
         Scanner scanner = new Scanner(System.in);
         boolean seguir = true;
         System.out.print("Ingrese la ruta del archivo: ");
@@ -13,28 +11,26 @@ public class ProyectoMain {
 
         String extension = caminoEntrada.substring(caminoEntrada.lastIndexOf('.') + 1).toLowerCase();
 
+        // CORRECCIÓN: Declaramos la variable 'buffer' fuera del if para que sea accesible en todo el método
         ImageBuffer buffer = null;
 
         if (extension.equals("jpg") || extension.equals("png")) {
-
-            buffer = new AdapterImagenJVM(caminoEntrada); 
+             buffer = new AdapterImagenJVM(caminoEntrada);
 
         } else if (extension.equals("pgm") || extension.equals("txt")) {
-            buffer = new AdapterImagenPGM(caminoEntrada); 
+             buffer = new AdapterImagenPGM(caminoEntrada);
 
         } else {
             System.out.println("Formato no soportado.");
             seguir = false;
         }
-
-        if (seguir && buffer != null) { 
+        if (seguir) {
             System.out.println("Ingrese el filtro que desea aplicar: ");
             System.out.println("1. Invertir colores");
             System.out.println("2. Escala de grises");
             System.out.println("3. Subir brillo (40 unidades)");
             int opcion = scanner.nextInt();
             FiltroImagen.TipoFiltro filtro;
-            
             if (opcion == 1) {
                 filtro = FiltroImagen.TipoFiltro.INVERTIR;
             }else if (opcion == 2) {
@@ -42,12 +38,11 @@ public class ProyectoMain {
             }else {
                 filtro = FiltroImagen.TipoFiltro.BRILLO;
             }
-            
-            ForkJoinPool pool = new ForkJoinPool();
+            ForkJoinPool pool = ForkJoinPool.commonPool();
             pool.invoke(new FiltroImagen(buffer.getPixeles(), 0, buffer.getPixeles().length, filtro));
             buffer.guardar("resultado." + extension);
-            System.out.println("Imagen guardada con éxito como: resultado." + extension);
             scanner.close();
+            
         }
     }
 }
